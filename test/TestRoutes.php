dd5d52f7094175df8d744b241cdd50376e9e1ff1
John Bintz <jcoswell@coswellproductions.org>
2009-02-24 14:38:00 -0500

    add more RoR test routes and add format route test

--- test/TestRoutes.php
+++ test/TestRoutes.php
@@ -22,8 +22,26 @@ class TestRoutes extends PHPUnit_Framework_TestCase {
 
         return array(
             array(":id", "(?P<id>{$pattern})"),
-            array("view/:id", "view/(?P<id>{$pattern})")
+            array("view/:id", "view/(?P<id>{$pattern})"),
+            array("view/:id1", "view/(?P<id1>{$pattern})"),
+            array("view/:1id", "view/(?P<1id>{$pattern})"),
+            array("view/:i_d", "view/(?P<i_d>{$pattern})"),
+            array("view/:i-d", "view/(?P<i>{$pattern})-d"),
+            array("view/:id/action", "view/(?P<id>{$pattern})/action"),
+            array("view/:id/action/:id2", "view/(?P<id>{$pattern})/action/(?P<id2>{$pattern})"),
+            array(":id:id2", "(?P<id>{$pattern})(?P<id2>{$pattern})")
         );
+    }
+
+    public function testFormatRoutes() {
+        $this->nicedog->url = "test";
+        $this->nicedog->add_url('', "Class", "method");
+        $this->assertEquals("/^$/", $this->nicedog->routes[0][0]);
+
+        $this->nicedog->routes = array();
+        $this->nicedog->url = "test.xml";
+        $this->nicedog->add_url('', "Class", "method");
+        $this->assertEquals("/^\.(?P<format>[a-zA-Z0-9]+)$/", $this->nicedog->routes[0][0]);
     }
 }
 
