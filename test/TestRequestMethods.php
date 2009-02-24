f5f5892e5c91b92409b9ab2dbfc1aca832937861
Scott Davis <jetviper21@gmail.com>
2009-02-24 15:29:56 -0500

    added Request test

---
+++ test/TestRequestMethods.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once('PHPUnit/Framework.php');
+require_once('../NiceDog.php');
+
+class TestRequests extends PHPUnit_Framework_TestCase {
+	
+	public function setUp() {
+			$_POST['_method'] = '';
+      $this->nicedog = NiceDog::getInstance();
+      $this->nicedog->routes = array();
+  }
+
+	public function testDelete() {
+		$_POST['_method'] = 'DELETE';
+ 		R('test/:id')->controller('Class')->action('method')->on('DELETE');
+		$this->assertEquals($this->nicedog->routes[0][3], $_POST['_method']);
+	}
+	
+	public function testPut() {
+		$_POST['_method'] = 'PUT';
+ 		R('test/:id')->controller('Class')->action('method')->on('PUT');
+		$this->assertEquals($this->nicedog->routes[0][3], $_POST['_method']);
+	}
+
+	
+}
+
+?>
