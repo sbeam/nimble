7b4e9510caaecbee679a99e1889ded60873b2639
Scott Davis <srdavis@scott-daviss-mac-mini.local>
2009-03-30 19:33:18 -0400

    cleaned up a broken line

--- lib/file_utils.php
+++ lib/file_utils.php
@@ -2,7 +2,9 @@
 
 class FileUtils {
 	
-	
+	/**
+	* @uses FileUtils::join('root', 'sub', 'nimble.txt')
+	*/
 	public static function join() {
 		$args = func_get_args();
 		return join(DIRECTORY_SEPARATOR, $args);
