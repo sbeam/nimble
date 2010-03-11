ed358c0b4b69dc44f60a091fd1fec546640fe337
Sam Beam <sbeam@onsetcorps.net>
2010-03-11 00:18:24 -0500

    load Inflector always

--- lib/support/base.php
+++ lib/support/base.php
@@ -1,28 +1,14 @@
 <?php
 	/**
 	* @package Support
-	* Loads in all support classes
+	* Loads in necessary support classes
 	*/
 require_once(dirname(__FILE__) . '/file_utils.php');
+require_once(dirname(__FILE__) . '/inflector.php');
 
-# $available_helpers = array('tag_helper', 'mime', 'inflector', 'string_cacher', 'asset_tag', 'cycler');
+# $available_helpers = array('tag_helper', 'mime', 'string_cacher', 'asset_tag', 'cycler');
 #
 function nimble_load_helper($name) {
-    require_once(FileUtils::join(dirname(__FILE__), $file . '.php'));
+    require_once(FileUtils::join(dirname(__FILE__), $name . '.php'));
 }
 
-	/**
-	* Similar to rubys collect method
-	* @param function $func
-	* @param array|interator $array
-	* @uses collect(function($value){return $value+1}, range(1,5));
-	*/
-	function collect($func, $array) {
-		$out = array();
-		foreach($array as $value) {
-			array_push($out, $func($value));
-		}
-		return $out;
-	}
-
-?>
