5b818ee803af4ce975b6b492e3934c4661e4ad66
Sam Beam <sbeam@onsetcorps.net>
2010-03-13 21:52:41 -0500

    use simplexml mode in XML_Serializer constructor

--- plugins/xml_view/init.php
+++ plugins/xml_view/init.php
@@ -5,7 +5,7 @@ require_once('XML/Serializer.php'); // PEAR
 class XMLview {
 
     public static function build($rootNode, $vals=null) {
-        $xsz = new XML_Serializer(array('rootName' => $rootNode));
+        $xsz = new XML_Serializer(array('rootName' => $rootNode, 'mode' => 'simplexml'));
         $xsz->serialize($vals);
         return $xsz->getSerializedData();
     }
